649fc24b7dd14f2179876423dfb67f80d2cbf00b
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-08-31 02:58:35 -0600

    responsive web 50 por ciento

--- src/components/LeagueForm.js
+++ src/components/LeagueForm.js
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import './LeagueFormStyles.css';
-
 
 const API_URL = 'https://backend-dlp-neuronube.koyeb.app/leagues';
 
@@ -11,14 +10,43 @@ const LeagueForm = ({ leagueToEdit = null, afterSave }) => {
   const [leagueFormat, setLeagueFormat] = useState(leagueToEdit?.league_format || '');
   const [startDate, setStartDate] = useState(leagueToEdit?.start_date?.split('T')[0] || '');
   const [startTime, setStartTime] = useState(leagueToEdit?.start_date?.split('T')[1]?.slice(0, 5) || '');
+  const [enlaceTorneo, setEnlaceTorneo] = useState(leagueToEdit?.enlace_torneo || '');
+  const [imageTorneo, setImageTorneo] = useState(leagueToEdit?.image_torneo || '');
+  const [infoTorneo, setInfoTorneo] = useState(leagueToEdit?.infoTorneo || [{
+    format: '',
+    banlist: '',
+    deck_info: '',
+    eliminacion: ''
+  }]);
+
+  useEffect(() => {
+    if (leagueToEdit) {
+      setLeagueName(leagueToEdit.league_name);
+      setLeagueFormat(leagueToEdit.league_format);
+      setStartDate(leagueToEdit.start_date.split('T')[0]);
+      setStartTime(leagueToEdit.start_date.split('T')[1]?.slice(0, 5));
+      setEnlaceTorneo(leagueToEdit.enlace_torneo);
+      setImageTorneo(leagueToEdit.image_torneo);
+      setInfoTorneo(leagueToEdit.infoTorneo);
+    }
+  }, [leagueToEdit]);
+
+  const handleInfoChange = (e, index, field) => {
+    const updatedInfo = [...infoTorneo];
+    updatedInfo[index][field] = e.target.value;
+    setInfoTorneo(updatedInfo);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const payload = { 
-        league_name: leagueName, 
-        league_format: leagueFormat, 
-        start_date: startDate + 'T' + startTime 
+      const payload = {
+        league_name: leagueName,
+        league_format: leagueFormat,
+        start_date: startDate + 'T' + startTime,
+        enlace_torneo: enlaceTorneo,
+        image_torneo: imageTorneo,
+        infoTorneo: infoTorneo,
       };
       let response;
 
@@ -36,6 +64,14 @@ const LeagueForm = ({ leagueToEdit = null, afterSave }) => {
       setLeagueFormat('');
       setStartDate('');
       setStartTime('');
+      setEnlaceTorneo('');
+      setImageTorneo('');
+      setInfoTorneo([{
+        format: '',
+        banlist: '',
+        deck_info: '',
+        eliminacion: ''
+      }]);
 
     } catch (error) {
       console.error("Error saving league:", error);
@@ -82,10 +118,61 @@ const LeagueForm = ({ leagueToEdit = null, afterSave }) => {
         />
       </div>
       <div className="leagueForm__group">
+        <label className="leagueForm__label">Enlace Torneo:</label>
+        <input
+          type="text"
+          value={enlaceTorneo}
+          onChange={(e) => setEnlaceTorneo(e.target.value)}
+          className="leagueForm__input"
+        />
+      </div>
+      <div className="leagueForm__group">
+        <label className="leagueForm__label">Image Torneo:</label>
+        <input
+          type="text"
+          value={imageTorneo}
+          onChange={(e) => setImageTorneo(e.target.value)}
+          className="leagueForm__input"
+        />
+      </div>
+      {infoTorneo.map((info, index) => (
+        <div key={index} className="leagueForm__group">
+          <label className="leagueForm__label">Format:</label>
+          <input
+            type="text"
+            value={info.format}
+            onChange={(e) => handleInfoChange(e, index, 'format')}
+            className="leagueForm__input"
+          />
+          <label className="leagueForm__label">Banlist:</label>
+          <input
+            type="text"
+            value={info.banlist}
+            onChange={(e) => handleInfoChange(e, index, 'banlist')}
+            className="leagueForm__input"
+          />
+          <label className="leagueForm__label">Deck Info:</label>
+          <input
+            type="text"
+            value={info.deck_info}
+            onChange={(e) => handleInfoChange(e, index, 'deck_info')}
+            className="leagueForm__input"
+          />
+          <label className="leagueForm__label">Eliminacion:</label>
+          <input
+            type="text"
+            value={info.eliminacion}
+            onChange={(e) => handleInfoChange(e, index, 'eliminacion')}
+            className="leagueForm__input"
+          />
+        </div>
+      ))}
+      <div className="leagueForm__group">
         <button type="submit" className="leagueForm__button">Save</button>
       </div>
     </form>
   );
+  
 };
 
 export default LeagueForm;
