25109c56664fa56fdce0af72ad828d323bc81202
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-09-04 08:02:36 -0600

    actualizacion reportes torneos

--- src/components/Torneos.js
+++ src/components/Torneos.js
@@ -10,13 +10,34 @@ function Torneos() {
   const [banner, setBanner] = useState('');
   const [top1, setTop1] = useState('');
   const [top2, setTop2] = useState('');
-  const [top3, setTop3] = useState('');
-  const [top4, setTop4] = useState('');
-  const [submitting, setSubmitting] = useState(false); // Estado para controlar el envío del formulario
-
+  const [top4_1, setTop4_1] = useState('');
+  const [top4_2, setTop4_2] = useState('');
+  const [top8_1, setTop8_1] = useState('');
+  const [top8_2, setTop8_2] = useState('');
+  const [top8_3, setTop8_3] = useState('');
+  const [top8_4, setTop8_4] = useState('');
+  const [decks, setDecks] = useState([{ nombre: '', cantidad: 0 }]);
+  const [submitting, setSubmitting] = useState(false);
+  
+  const handleAddDeck = () => {
+    setDecks([...decks, { nombre: '', cantidad: 0 }]);
+  };
+  
+  const handleRemoveDeck = (index) => {
+    const newDecks = [...decks];
+    newDecks.splice(index, 1);
+    setDecks(newDecks);
+  };
+  
+  const handleDeckChange = (e, index, field) => {
+    const newDecks = [...decks];
+    newDecks[index][field] = e.target.value;
+    setDecks(newDecks);
+  };
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+    
     const torneoData = {
       nombre,
       fecha,
@@ -26,25 +47,28 @@ function Torneos() {
       banner,
       top1,
       top2,
-      top3,
-      top4
+      top4_1,
+      top4_2,
+      top8_1,
+      top8_2,
+      top8_3,
+      top8_4,
+      decks
     };
 
     try {
-      setSubmitting(true); // Establecer el estado de envío en true durante la solicitud
-
-      const response = await fetch('https://back-render-cloud-dlp.onrender.com/torneos', {
+      setSubmitting(true);
+  
+      const response = await fetch('https://backend-dlp-neuronube.koyeb.app/torneos', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(torneoData),
       });
-
+  
       if (response.ok) {
-        // El torneo se creó exitosamente
         console.log('Torneo creado');
-        // Restablecer los campos a sus valores iniciales
         setNombre('');
         setFecha('');
         setOrganizador('');
@@ -53,16 +77,20 @@ function Torneos() {
         setBanner('');
         setTop1('');
         setTop2('');
-        setTop3('');
-        setTop4('');
+        setTop4_1('');
+        setTop4_2('');
+        setTop8_1('');
+        setTop8_2('');
+        setTop8_3('');
+        setTop8_4('');
+        setDecks([{ nombre: '', cantidad: 0 }]);
       } else {
-        // Ocurrió un error al crear el torneo
         console.log('Error al crear el torneo');
       }
     } catch (error) {
       console.error(error);
     } finally {
-      setSubmitting(false); // Establecer el estado de envío en false después de la solicitud
+      setSubmitting(false);
     }
   };
 
@@ -72,92 +100,80 @@ function Torneos() {
       <form className="torneos-form" onSubmit={handleSubmit}>
         <div className="form-group">
           <label className="form-label">Nombre:</label>
-          <input
-            type="text"
-            className="form-input"
-            value={nombre}
-            onChange={(e) => setNombre(e.target.value)}
-          />
+          <input type="text" className="form-input" value={nombre} onChange={(e) => setNombre(e.target.value)} />
         </div>
         <div className="form-group">
           <label className="form-label">Fecha:</label>
-          <input
-            type="text"
-            className="form-input"
-            value={fecha}
-            onChange={(e) => setFecha(e.target.value)}
-          />
+          <input type="text" className="form-input" value={fecha} onChange={(e) => setFecha(e.target.value)} />
         </div>
         <div className="form-group">
           <label className="form-label">Organizador:</label>
-          <input
-            type="text"
-            className="form-input"
-            value={organizador}
-            onChange={(e) => setOrganizador(e.target.value)}
-          />
+          <input type="text" className="form-input" value={organizador} onChange={(e) => setOrganizador(e.target.value)} />
         </div>
         <div className="form-group">
           <label className="form-label">Información del Torneo:</label>
-          <textarea
-            className="form-textarea"
-            value={informacionTorneo}
-            onChange={(e) => setInformacionTorneo(e.target.value)}
-          ></textarea>
+          <textarea className="form-textarea" value={informacionTorneo} onChange={(e) => setInformacionTorneo(e.target.value)}></textarea>
         </div>
         <div className="form-group">
           <label className="form-label">Formato del Torneo:</label>
-          <input
-            type="text"
-            className="form-input"
-            value={formatoTorneo}
-            onChange={(e) => setFormatoTorneo(e.target.value)}
-          />
+          <input type="text" className="form-input" value={formatoTorneo} onChange={(e) => setFormatoTorneo(e.target.value)} />
         </div>
         <div className="form-group">
           <label className="form-label">Banner:</label>
-          <input
-            type="text"
-            className="form-input"
-            value={banner}
-            onChange={(e) => setBanner(e.target.value)}
-          />
+          <input type="text" className="form-input" value={banner} onChange={(e) => setBanner(e.target.value)} />
         </div>
         <div className="form-group">
           <label className="form-label">Top 1:</label>
-          <input
-            type="text"
-            className="form-input"
-            value={top1}
-            onChange={(e) => setTop1(e.target.value)}
-          />
+          <input type="text" className="form-input" value={top1} onChange={(e) => setTop1(e.target.value)} />
         </div>
         <div className="form-group">
           <label className="form-label">Top 2:</label>
-          <input
-            type="text"
-            className="form-input"
-            value={top2}
-            onChange={(e) => setTop2(e.target.value)}
-          />
+          <input type="text" className="form-input" value={top2} onChange={(e) => setTop2(e.target.value)} />
         </div>
         <div className="form-group">
-          <label className="form-label">Top 3:</label>
-          <input
-            type="text"
-            className="form-input"
-            value={top3}
-            onChange={(e) => setTop3(e.target.value)}
-          />
+          <label className="form-label">Top 4-1:</label>
+          <input type="text" className="form-input" value={top4_1} onChange={(e) => setTop4_1(e.target.value)} />
         </div>
         <div className="form-group">
-          <label className="form-label">Top 4:</label>
-          <input
-            type="text"
-            className="form-input"
-            value={top4}
-            onChange={(e) => setTop4(e.target.value)}
-          />
+          <label className="form-label">Top 4-2:</label>
+          <input type="text" className="form-input" value={top4_2} onChange={(e) => setTop4_2(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label className="form-label">Top 8-1:</label>
+          <input type="text" className="form-input" value={top8_1} onChange={(e) => setTop8_1(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label className="form-label">Top 8-2:</label>
+          <input type="text" className="form-input" value={top8_2} onChange={(e) => setTop8_2(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label className="form-label">Top 8-3:</label>
+          <input type="text" className="form-input" value={top8_3} onChange={(e) => setTop8_3(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label className="form-label">Top 8-4:</label>
+          <input type="text" className="form-input" value={top8_4} onChange={(e) => setTop8_4(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label className="form-label">Decks:</label>
+          {decks.map((deck, index) => (
+            <div key={index}>
+              <input
+                type="text"
+                placeholder="Nombre del Deck"
+                value={deck.nombre}
+                onChange={(e) => handleDeckChange(e, index, 'nombre')}
+              />
+              <input
+                type="number"
+                placeholder="Cantidad"
+                value={deck.cantidad}
+                onChange={(e) => handleDeckChange(e, index, 'cantidad')}
+              />
+              <button type="button" onClick={() => handleRemoveDeck(index)}>Eliminar</button>
+            </div>
+          ))}
+          <button type="button" onClick={handleAddDeck}>Agregar Deck</button>
         </div>
         <button type="submit" className="submit-button" disabled={submitting}>
           {submitting ? 'Enviando...' : 'Crear Torneo'}
